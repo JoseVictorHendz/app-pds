de2a3081815f2dc4dbeba337e4e697ec6bb341a9
Jose Victor Hendz Martins <Jose>
2018-11-06 19:25:08 -0200

    add convert base64

--- app/src/main/java/comjosevictorhendz/httpsgithub/apppds/MainActivity.kt
+++ app/src/main/java/comjosevictorhendz/httpsgithub/apppds/MainActivity.kt
@@ -6,12 +6,17 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import 	android.provider.MediaStore
 import android.content.Intent
+import android.graphics.Bitmap
 import android.widget.Button
+import android.widget.ImageView
+import android.util.Base64
+import java.io.ByteArrayOutputStream
 import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
     internal lateinit var image_label_detection: Button
+    internal lateinit var mImageView: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,19 +25,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         image_label_detection = findViewById(R.id.image_label_detection)
+        mImageView = findViewById(R.id.mImageView)
 
-
-
-
-        val REQUEST_IMAGE_CAPTURE = 1
-
-        fun dispatchTakePictureIntent() {
-            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-                takePictureIntent.resolveActivity(packageManager)?.also {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                }
-            }
-        }
 
         image_label_detection.setOnClickListener {
             dispatchTakePictureIntent()
@@ -41,9 +35,44 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data!!.extras.get("data") as Bitmap
+            mImageView.setImageBitmap(imageBitmap)
+
+
+            try {
+                val encodedImage = encodeImage(imageBitmap)
+                Toast.makeText(applicationContext, "Bombando: " + encodedImage, Toast.LENGTH_LONG).show()
+            }catch (exeption: Exception) {
+                Toast.makeText(applicationContext, "error: " + exeption, Toast.LENGTH_LONG).show()
+
+            }
+
+
+
+
+        }
+    }
+
+    private fun encodeImage(bm: Bitmap): String {
+        val ByteArray = ByteArrayOutputStream()
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, ByteArray)
+        val b = ByteArray.toByteArray()
+
+        return Base64.encodeToString(b, Base64.DEFAULT)
+    }
 
 }
-
-
-
-
